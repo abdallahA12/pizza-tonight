1ced9fc664c3ca66df696a3e6cab43cfadb2c1c7
Adam Abdallah <khalizatelli@gmail.com>
2019-11-10 07:13:02 +0300

    adding pizza prices

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ function Order (customSize, cheese) {
   this.pizzaPrice = 150;
   this.sidePrice = 150;
 }
+Order.prototype.pizzaCost = function () {
+    if (this.customSize === "Small 10 in.") {
+      this.pizzaPrice += 6;
+    } else if (this.customSize === "Medium 14 in.") {
+      this.pizzaPrice += 9;
+    } else if (this.customSize === "Large 18 in.") {
+      this.pizzaPrice += 12;
+    }
+    if (this.cheese === "cheese") {
+      this.pizzaPrice += 1;
+    } else if (this.cheese === "light cheese") {
+      this.pizzaPrice += 0.5;
+    } else if (this.cheese === "extra cheese") {
+      this.pizzaPrice += 1.5;
+    }
+    this.pizzaPrice += this.sauce;
+    this.pizzaPrice += this.veggie1;
+    this.pizzaPrice += this.veggie2;
+    this.pizzaPrice += this.meat;
+    return this.pizzaPrice;
+  }
